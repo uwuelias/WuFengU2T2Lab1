0595567a0f8fe1856880d12a235a83e872d5f381
Elias Wu Feng <eliasw10@nycstudents.net>
2022-09-30 11:19:23 -0400

    Initial commit

---
+++ src/CircleRunner.java
@@ -0,0 +1,15 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circle1= new Circle(5.0);
+        circle1.printArea();
+
+        // A. create a Circle object with radius 5.0
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+
+        // C. BEFORE running this code, PREDICT what gets printed
+
+        // D. run the code and confirm your prediction! Were you correct?
+    }
+}
